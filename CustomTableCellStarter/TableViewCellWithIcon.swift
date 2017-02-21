33d851b88284becf048a806ff9ad18a0f97543f7
Chris Chadillon <chris@chadillon.com>
2017-02-21 13:48:25 -0500

    1) Added 2 Custom Table View Cell Classes

---
+++ CustomTableCellStarter/TableViewCellWithIcon.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCellWithIcon.swift
+//  CustomTableCellStarter
+//
+//  Created by Chris Chadillon on 2017-02-21.
+//  Copyright © 2017 Chris Chadillon. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellWithIcon: UITableViewCell {
+
+}
---
+++ CustomTableCellStarter/TableViewCellWithoutIcon.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCellWithoutIcon.swift
+//  CustomTableCellStarter
+//
+//  Created by Chris Chadillon on 2017-02-21.
+//  Copyright © 2017 Chris Chadillon. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellWithoutIcon: UITableViewCell {
+
+}
+
