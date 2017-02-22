b10a25c212ca1799ee88c9c4b5fa8b62707b39e4
Chris Chadillon <chris@chadillon.com>
2017-02-22 13:09:00 -0500

    1) Added a basic VC with a Cancel button that dismisses the VC 2) Added 2 subways, one for each custom cell in the TVC

---
+++ CustomTableCellStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableCellStarter
+//
+//  Created by Chris Chadillon on 2017-02-22.
+//  Copyright © 2017 Chris Chadillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doCancel(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
