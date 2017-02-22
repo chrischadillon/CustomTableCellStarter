842709586cb6e312e0b08575b1557c2bbc98c41e
Chris Chadillon <chris@chadillon.com>
2017-02-22 13:26:43 -0500

    1) Added a 2nd VC 2) Set a segue from the Icon cell to the new VC

---
+++ CustomTableCellStarter/IconViewController.swift
@@ -0,0 +1,21 @@
+//
+//  IconViewController.swift
+//  CustomTableCellStarter
+//
+//  Created by Chris Chadillon on 2017-02-22.
+//  Copyright © 2017 Chris Chadillon. All rights reserved.
+//
+
+import UIKit
+
+class IconViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func doCancel(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
